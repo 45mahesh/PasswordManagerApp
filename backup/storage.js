790d69dd914dc7b59e3b5ccf4e24d29940f8bd85
45mahesh <bhimdemahesh@gmail.com>
2025-10-19 17:37:06 +0530

    Remove build artifacts and sensitive files

--- backup/storage.js
+++
@@ -1,9 +0,0 @@
-import * as SecureStore from 'expo-secure-store';
-
-export async function savePassword(key, value) {
-  await SecureStore.setItemAsync(key, value);
-}
-
-export async function getPassword(key) {
-  return await SecureStore.getItemAsync(key);
-}
